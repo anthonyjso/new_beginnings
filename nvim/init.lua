44415123a2cdd7650597df0340c4d799ad2ed235
Anthony So <anthonyjso@users.noreply.github.com>
2024-05-26 21:11:48 -0400

    Initial commits for nvim lua config

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+-- OPTIONS
+-- tabs
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+
+-- line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- KEYMAPS
+vim.g.mapleader = " "
