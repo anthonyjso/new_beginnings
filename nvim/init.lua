3132b119fa2cfdf1d186cae3bd6b69e914347ef3
Anthony So <anthonyjso@users.noreply.github.com>
2024-05-28 16:00:16 -0400

    Nvim trim whitespace and update keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -72,44 +72,58 @@ require("lazy").setup(plugins, opts)
 -- LSPs
 local lspconfig = require('lspconfig')
 
+lspconfig.bashls.setup{}
+
 -- brew install lua-language-server
 -- https://github.com/neovim/neovim/issues/21686#issuecomment-1522446128
 lspconfig.lua_ls.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {
-          'vim',
-          'require'
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {
+                    'vim',
+                    'require'
+                },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
         },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
     },
+}
+
+lspconfig.tsserver.setup{
+  init_options = {
+    plugins = {},
+  },
+  filetypes = {
+    "javascript",
+    "typescript",
   },
 }
+
 
 -- COLORSCHEME
 vim.cmd("colorscheme rose-pine-moon")
 
 -- telescope.nvim
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff' , builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fp', builtin.git_files, {})
+vim.keymap.set('n', '<leader>c', builtin.commands, {})
 
 -- trim trailing whitespace on save
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
@@ -120,3 +134,23 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
         vim.fn.setpos(".", save_cursor)
     end,
 })
+
+-- Function to open init.lua
+function OpenInitLua()
+  local config_path = vim.fn.stdpath('config') .. '/init.lua'
+  vim.cmd('edit ' .. config_path)
+end
+vim.api.nvim_create_user_command('Config', OpenInitLua, {desc="Search available commands"})
+
+-- Function to format the current buffer
+function FormatCurrentBuffer()
+  vim.lsp.buf.format({ async = true })
+end
+
+-- Map COMMAND+ALT+l to format the current buffer
+vim.api.nvim_set_keymap(
+  'n',
+  '<leader>l',
+  ':lua FormatCurrentBuffer()<CR>',
+  { noremap = true, silent = true }
+)
