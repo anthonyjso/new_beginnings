48a6abf3afee80e0d4549eeaebd1bc3b44d7fb09
Anthony So <anthonyjso@users.noreply.github.com>
2024-05-27 23:29:28 -0400

    Move nvim config to lua based setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,9 +6,78 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 
+
 -- line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+
 -- KEYMAPS
 vim.g.mapleader = " "
+
+-- PLUGINS
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    {"neovim/nvim-lspconfig"},
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {"nvim-telescope/telescope.nvim", tag = "0.1.6", dependencies = {"nvim-lua/plenary.nvim" }},
+    {"rose-pine/neovim", name = "rose-pine"},
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+-- LSPs
+local lspconfig = require('lspconfig')
+
+-- brew install lua-language-server
+-- https://github.com/neovim/neovim/issues/21686#issuecomment-1522446128
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          'vim',
+          'require'
+        },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
+-- COLORSCHEME
+vim.cmd("colorscheme rose-pine-moon")
+
+-- telescope.nvim
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
