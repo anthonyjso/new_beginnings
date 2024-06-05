4f7c488f2b5a1fcca5346df5330b4450ec7b751c
Anthony So <anthonyjso@users.noreply.github.com>
2024-06-04 23:25:44 -0400

    Nvim: Refactored after issues with nvm-cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,10 +7,22 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 vim.opt.nu = true
 vim.opt.relativenumber = true
-
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
 
 -- KEYMAPS
+-- Map leader to space
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+--- Function to open init.lua
+local function OpenInitLua()
+  local config_path = vim.fn.stdpath("config") .. "/init.lua"
+  vim.cmd("edit " .. config_path)
+end
+vim.api.nvim_create_user_command("Config", OpenInitLua, { desc = "Search available commands" })
+vim.keymap.set("n", "<M-,>", "<cmd>Config<cr>")
 
 -- PLUGINS
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -24,200 +36,311 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     lazypath,
   })
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.runtimepath:prepend(lazypath)
 
 local plugins = {
-  { "neovim/nvim-lspconfig" },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  { "nvim-telescope/telescope.nvim",   tag = "0.1.6",      dependencies = { "nvim-lua/plenary.nvim" } },
-  { "junegunn/goyo.vim" },
-  { "tpope/vim-fugitive" },
-  { "Exafunction/codeium.vim" },
-  { "rose-pine/neovim",                name = "rose-pine" },
   {
-    "hrsh7th/nvim-cmp",
-    -- load cmp on InsertEnter
-    event = "InsertEnter",
-    -- these dependencies will only be loaded when cmp loads
-    -- dependencies are always lazy-loaded unless specified otherwise
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 50,
+  },
+  "folke/which-key.nvim",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      ensure_installed = {
+        "bash",
+        "diff",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      },
+    },
+  },
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-cmdline",
       "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lua",
-      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- Library items can be absolute paths
+            -- "~/projects/my-awesome-lib",
+            -- Or relative, which means they will be resolved as a plugin
+            -- "LazyVim",
+            -- When relative, you can also provide a path to the library in the plugin dir
+            "luvit-meta/library", -- see below
+          },
+        },
+      },
     },
     config = function ()
+      local lspconfig = require("lspconfig")
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+      -- Add additional capabilities supported by nvim-cmp
+      local capabilities = cmp_nvim_lsp.default_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+      -- bashls depends on shellcheck and shellfmt
+      local servers = { "bashls", "cssls", "html", "jsonls", "pyright", "ruff" }
+      for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup { capabilities = capabilities }
+      end
+
+      lspconfig.lua_ls.setup {
+        capabilities = capabilities,
+        settings = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+          completion = {
+            callSnippet = "Replace",
+          }
+        },
+      }
+      -- vscode-langservers-extracted
+      lspconfig.tsserver.setup {
+        capabilities = capabilities,
+        init_options = {
+          plugins = {},
+        },
+        filetypes = {
+          "javascript",
+          "typescript",
+        },
+      }
+
+      -- Configure keymaps and autocommands to use LSP features.
+      -- https://neovim.io/doc/user/lsp.html#lsp-quickstart
+      local AsoGroup = vim.api.nvim_create_augroup("AsoGroup", {})
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = AsoGroup,
+        callback = function (e)
+          local lsp_opts = { buffer = e.buf }
+          vim.keymap.set("n", "gd", function () vim.lsp.buf.definition() end, lsp_opts)
+          vim.keymap.set("n", "gD", function () vim.lsp.buf.declaration() end, lsp_opts)
+          vim.keymap.set("n", "<leader>vd", function () vim.diagnostic.open_float() end, lsp_opts)
+          vim.keymap.set("n", "K", function () vim.lsp.buf.hover() end, lsp_opts)
+          vim.keymap.set("n", "gK", function () vim.lsp.buf.signature_help() end, lsp_opts)
+          vim.keymap.set("i", "<c-k>", function () vim.lsp.buf.signature_help() end, lsp_opts)
+          vim.keymap.set("n", "<leader>ca", function () vim.lsp.buf.code_action() end, lsp_opts)
+          vim.keymap.set("n", "<leader>l", function () vim.lsp.buf.format({ async = true }) end, lsp_opts)
+          vim.keymap.set("n", "]]", function () vim.diagnostic.goto_next() end, lsp_opts)
+          vim.keymap.set("n", "[[", function () vim.diagnostic.goto_prev() end, lsp_opts)
+        end
+      })
+    end
+  },
+  {
+    -- optional `vim.uv` typings
+    "Bilal2453/luvit-meta",
+    lazy = true
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-nvim-lua",
+      -- luasnip
+      {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+      },
+      "saadparwaiz1/cmp_luasnip", -- for autocompletion
+      "onsails/lspkind.nvim",     -- vs-code like pictograms
+    },
+    opts = function (_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, {
+        name = "lazydev",
+        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+      })
+    end,
+    config = function ()
       local cmp = require("cmp")
-      vim.opt.completeopt = { "menu", "menuone", "noselect" }
+      require("luasnip.loaders.from_vscode").lazy_load()
+      local luasnip = require("luasnip")
+      local lspkind = require("lspkind")
 
       cmp.setup({
         window = {
-          -- completion = cmp.config.window.bordered(),
-          -- documentation = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        snippet = {
+          expand = function (args)
+            luasnip.lsp_expand(args.body)
+          end,
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<leader><Tab>"] = cmp.mapping.complete(),
+          ["<C-p>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-n>"] = cmp.mapping.scroll_docs(4),
+          ["<C-a>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function (fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function (fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "nvim_lsp_document_symbol" },
-        }, {
+          { name = "luasnip" }, -- snippets
           { name = "buffer" },
-          { name = "path" },
+          { name = "nvim_lsp_signature_help" },
         }),
+        formatting = {
+          fields = { "menu", "abbr", "kind" },
+          expandable_indicator = true,
+          format = lspkind.cmp_format({
+            maxwidth = 50,
+            ellipsis_char = "...",
+          })
+        },
       })
 
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" }
+        }
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
       cmp.setup.cmdline(":", {
         mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({
           { name = "path" },
         }, {
-          { name = "cmdline" },
+          { name = "cmdline" }
         }),
       })
     end
-
   },
-}
-
-require("lazy").setup(plugins, {})
-
--- LSPs
-local lspconfig = require("lspconfig")
-
--- Depends on shellcheck and shellfmt
-lspconfig.bashls.setup {}
-
--- brew install lua-language-server
--- https://github.com/neovim/neovim/issues/21686#issuecomment-1522446128
-lspconfig.lua_ls.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
-        version = "LuaJIT",
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = "Telescope",
+    keys = {
+      {
+        "<leader>ff",
+        "<cmd>Telescope find_files<cr>",
+        desc = "Find files",
       },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {
-          "vim",
-          "require"
-        },
+      {
+        "<leader>fg",
+        "<cmd>Telescope live_grep<cr>",
+        desc = "Live grep",
       },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
+      {
+        "<leader>fb",
+        "<cmd>Telescope buffers<cr>",
+        desc = "Find buffers",
       },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
+      {
+        "<leader>fh",
+        "<cmd>Telescope help_tags<cr>",
+        desc = "Help tags",
+      },
+
+    }
+  },
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
       },
     },
   },
-}
-
--- harper-ls
--- lspconfig.harper_ls.setup {
---   settings = {
---     ["harper-ls"] = {
---       linters = {
---         spell_check = true,
---         spelled_numbers = false,
---         an_a = true,
---         sentence_capitalization = true,
---         unclosed_quotes = true,
---         wrong_quotes = false,
---         long_sentences = true,
---         repeated_words = true,
---         spaces = true,
---         matcher = true,
---         correct_number_suffix = true,
---         number_suffix_capitalization = true,
---         userDictPath = "~/.local/share/nvim/dict.txt"
---       }
---     }
---   },
--- }
-
-
--- vscode-langservers-extracted
-lspconfig.cssls.setup {}
-lspconfig.html.setup {}
-lspconfig.jsonls.setup {}
-lspconfig.pyright.setup {}
-lspconfig.ruff.setup {}
-lspconfig.tsserver.setup {
-  init_options = {
-    plugins = {},
-  },
-  filetypes = {
-    "javascript",
-    "typescript",
+  {
+    "folke/zen-mode.nvim",
+    config = function ()
+      local zen_mode = require("zen-mode")
+      vim.keymap.set("n", "<leader>zm",
+        function ()
+          zen_mode.toggle({ window = { backdrop = 0 }, })
+        end)
+    end
   },
 }
 
--- Configure keymaps and autocommands to use LSP features.
--- https://neovim.io/doc/user/lsp.html#lsp-quickstart
-local AsoGroup = vim.api.nvim_create_augroup("AsoGroup", {})
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = AsoGroup,
-  callback = function (e)
-    local lsp_opts = { buffer = e.buf }
-    vim.keymap.set("n", "gd", function () vim.lsp.buf.definition() end, lsp_opts)
-    vim.keymap.set("n", "gD", function () vim.lsp.buf.declaration() end, lsp_opts)
-    vim.keymap.set("n", "K", function () vim.lsp.buf.hover() end, lsp_opts)
-    vim.keymap.set("n", "gK", function () vim.lsp.buf.signature_help() end, lsp_opts)
-    vim.keymap.set("i", "<c-k>", function () vim.lsp.buf.signature_help() end, lsp_opts)
-    vim.keymap.set("n", "<leader>ca", function () vim.lsp.buf.code_action() end, lsp_opts)
-    vim.keymap.set("n", "gr", function () vim.lsp.buf.references() end, lsp_opts)
-    vim.keymap.set("n", "<leader>l", function () vim.lsp.buf.format({ async = true }) end, lsp_opts)
-    vim.keymap.set("n", "<leader>cR", function () vim.lsp.buf.rename() end, lsp_opts)
-    vim.keymap.set("n", "]]", function () vim.diagnostic.goto_next() end, lsp_opts)
-    vim.keymap.set("n", "[[", function () vim.diagnostic.goto_prev() end, lsp_opts)
-    vim.keymap.set("n", "<a-n>", function () vim.diagnostic.goto_next() end, lsp_opts)
-    vim.keymap.set("n", "<a-p>", function () vim.diagnostic.goto_prev() end, lsp_opts)
-  end
-})
-
--- telescope.nvim
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fp", builtin.git_files, {})
-vim.keymap.set("n", "<leader>c", builtin.commands, {})
-vim.keymap.set("n", "<M-space>", builtin.lsp_definitions, {})
-
-
--- trim trailing whitespace on save
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*" },
-  callback = function ()
-    local save_cursor = vim.fn.getpos(".")
-    vim.cmd([[%s/\s\+$//e]])
-    vim.fn.setpos(".", save_cursor)
-  end,
-})
-
--- Function to open init.lua
-function OpenInitLua()
-  local config_path = vim.fn.stdpath("config") .. "/init.lua"
-  vim.cmd("edit " .. config_path)
-end
-
-vim.api.nvim_create_user_command("Config", OpenInitLua, { desc = "Search available commands" })
-
-
--- Codeium https://github.com/Exafunction/codeium.vim
--- Enable with CodeiumEnable if needed
-vim.g.codeium_enabled = false
-
--- COLORSCHEME
+local lazy = require("lazy")
+lazy.setup(plugins, { colorscheme = { "rose-pine-moon" } })
 vim.cmd("colorscheme rose-pine-moon")
